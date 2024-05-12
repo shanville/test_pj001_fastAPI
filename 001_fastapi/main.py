67ba73355ab158d6ac71606fbb1d1595e7ad377f
shanville <yuta.syukatsu@gmail.com>
2024-05-12 19:55:45 +0900

    second commit

--- 001_fastapi/main.py
+++ 001_fastapi/main.py
@@ -13,6 +13,7 @@ def get_db():
         yield db
     finally:
         db.close()
+        
 
 @app.post("/users/", response_model=UserCreate)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
